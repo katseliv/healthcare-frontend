66a6f088beb476de10aa677d2c7a41ae0e64ecd3
BrenevAlexey <brenevalexey@yandex.ru>
2021-07-26 23:06:55 +0300

    Avatar and info added

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="profile">
     <div class="profile__avatar">
-      <el-avatar shape="circle" :size="200" :fit="fit">
+      <el-avatar shape="circle" :size="175" :fit="fit">
         <img :src="src" alt="avatar" />
       </el-avatar>
       <h4>{{ surname }} {{ name }} {{ parentName }}</h4>
+      <p><span>Sex:</span> {{ sex }}</p>
+      <p><span>Email:</span> {{ eMail }}</p>
+      <p><span>Age:</span> {{ age }}</p>
     </div>
     <el-tabs
       class="profile__tabs"
       v-model="activeName"
       @tab-click="handleClick"
     >
-      <el-tab-pane class="profile__tab" label="User" name="first">
+      <el-tab-pane class="profile__tab" label="Visits" name="first">
       </el-tab-pane>
-      <el-tab-pane class="profile__tab" label="Config" name="second"
+      <el-tab-pane class="profile__tab" label="Diseases" name="second"
         >Config</el-tab-pane
       >
-      <el-tab-pane class="profile__tab" label="Role" name="third"
+      <el-tab-pane class="profile__tab" label="Allergies" name="third"
         >Role</el-tab-pane
       >
-      <el-tab-pane class="profile__tab" label="Task" name="fourth"
-        >Task</el-tab-pane
+      <el-tab-pane class="profile__tab" label="Settings" name="fourth"
+        >Settings</el-tab-pane
       >
     </el-tabs>
   </div>
@@ -38,9 +41,9 @@ export default defineComponent({
       name: "Man",
       surname: "Florida",
       parentName: "",
-      sex: "",
-      eMail: "",
-      age: "",
+      sex: "Male",
+      eMail: "man@florida.com",
+      age: "35",
     };
   },
 });
@@ -50,19 +53,39 @@ export default defineComponent({
 .profile {
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
   width: 100%;
   min-height: 90vh;
   min-height: calc(100vh - 110px);
 }
 
 .profile__avatar {
-  flex: 0 0 30%;
+  flex: 1 0 20%;
   text-align: center;
-  border-right: 1px solid #c4c4c4;
+  border-right: 2px solid #e4e7ed;
   margin-right: 30px;
+  padding-right: 10px;
 }
 .profile__tabs {
-  flex: 0 1 70%;
+  flex: 0 1 80%;
+}
+p {
+  text-align: left;
+}
+p span {
+  font-weight: bold;
+  padding-right: 5px;
+}
+@media (max-width: 576px) {
+  .profile {
+    flex-direction: column;
+  }
+  .profile__avatar {
+    flex: 0 0 100%;
+    margin-right: 0;
+    margin-bottom: 2px solid #e4e7ed;
+  }
+  .profile__tabs {
+    flex: 0 0 100%;
+  }
 }
 </style>
