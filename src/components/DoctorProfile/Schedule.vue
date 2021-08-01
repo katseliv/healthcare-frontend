1bdfa95b9500de944f9694b4d7151918fa9013e3
BrenevAlexey <brenevalexey@yandex.ru>
2021-08-02 02:29:20 +0300

    styles added to scheldule

--- src/components/DoctorProfile/Schedule.vue
+++ src/components/DoctorProfile/Schedule.vue
@@ -4,8 +4,14 @@
     v-for="day in schedule"
     :key="day.id"
     :data="day.slots"
+    :row-class-name="tableRowClassName"
+    style="max-width: 400px"
   >
-    <el-table-column :label="day.day" prop="time"></el-table-column>
+    >
+    <el-table-column :label="day.day" prop="time" width="200"></el-table-column>
+    <el-table-column label="Действия">
+      <el-button icon="el-icon-delete" size="small">Удалить</el-button>
+    </el-table-column>
   </el-table>
 </template>
 
@@ -13,6 +19,16 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  methods: {
+    tableRowClassName({ row }: any) {
+      if (row.isFree === true) {
+        return "success-row";
+      } else {
+        return "warning-row";
+      }
+    },
+  },
+
   data() {
     return {
       schedule: [
@@ -21,15 +37,18 @@ export default defineComponent({
           slots: [
             {
               id: 1,
-              time: "15:20",
+              time: "124:20",
+              isFree: false,
             },
             {
               id: 2,
               time: "16:40",
+              isFree: true,
             },
             {
               id: 3,
               time: "17:45",
+              isFree: true,
             },
           ],
         },
@@ -39,14 +58,17 @@ export default defineComponent({
             {
               id: 4,
               time: "15:20",
+              isFree: true,
             },
             {
               id: 5,
               time: "16:40",
+              isFree: true,
             },
             {
               id: 6,
               time: "17:45",
+              isFree: true,
             },
           ],
         },
@@ -56,14 +78,17 @@ export default defineComponent({
             {
               id: 7,
               time: "15:20",
+              isFree: true,
             },
             {
               id: 8,
               time: "16:40",
+              isFree: true,
             },
             {
               id: 9,
               time: "17:45",
+              isFree: true,
             },
           ],
         },
@@ -73,14 +98,17 @@ export default defineComponent({
             {
               id: 10,
               time: "15:20",
+              isFree: true,
             },
             {
               id: 11,
               time: "16:40",
+              isFree: true,
             },
             {
               id: 12,
               time: "17:45",
+              isFree: true,
             },
           ],
         },
@@ -90,14 +118,17 @@ export default defineComponent({
             {
               id: 13,
               time: "15:20",
+              isFree: true,
             },
             {
               id: 14,
               time: "16:40",
+              isFree: true,
             },
             {
               id: 15,
               time: "17:45",
+              isFree: true,
             },
           ],
         },
@@ -107,14 +138,17 @@ export default defineComponent({
             {
               id: 16,
               time: "15:20",
+              isFree: true,
             },
             {
               id: 17,
               time: "16:40",
+              isFree: true,
             },
             {
               id: 18,
               time: "17:45",
+              isFree: true,
             },
           ],
         },
@@ -124,14 +158,17 @@ export default defineComponent({
             {
               id: 19,
               time: "15:20",
+              isFree: true,
             },
             {
               id: 20,
               time: "16:40",
+              isFree: true,
             },
             {
               id: 21,
               time: "17:45",
+              isFree: true,
             },
           ],
         },
@@ -141,10 +178,21 @@ export default defineComponent({
 });
 </script>
 
+<style>
+.el-table .success-row {
+  background-color: #9fe99fc7;
+}
+
+.el-table .warning-row {
+  background-color: #c07c7cd5;
+}
+</style>
+
+
 <style scoped>
 @media (max-width: 720px) {
-}
-.table {
-  overflow-x: scroll;
+  .table {
+    overflow-x: scroll;
+  }
 }
 </style>
