84926a40dfaf5a8f319146f0f883d5370c45a897
Brenev Alexey <BrenevAlexey@yandex.ru>
2021-08-07 20:12:36 +0300

    dance with a tambourine 2

--- src/api/EventService.ts
+++ src/api/EventService.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const apiClient = axios.create({
     // baseURL: 'http://localhost:8081/api',
-    baseURL: 'http://servachok2021.ddns.net:8081/api',
+    baseURL: 'http://servachok2021.ddns.net:8080',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
